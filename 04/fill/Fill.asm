bad36240e854d5428001b62ba1373956e4c1d86c
maddiehulcy <60118685+maddiehulcy@users.noreply.github.com>
2021-03-29 19:29:21 -0500

    going to try different approach

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -13,42 +13,39 @@
 
 // Put your code here.
 
-(BEGIN)
-	@SCREEN
-	D = A
-	@curr
-	M = D
+(INIT)  	//initializes i - index that runs on the screen's pixels
+	@8192	 // 32 * 256 number of 16 bit pixel lines to cover the entire screen
+	D=A
+	@i                   //initializes the index variable to 8192, this is the remaining address left to color onscreen
+	M=D
 
-(LOOP)
-	@IN
-	D = M
-	@WHITE
-	D;JEQ //if no input, white
-	@BLACK
-	0;JMP //if input, black
-
-(BLACK)
-	@curr
-	M = -1
-	D = M + 1
-	M = D //go to next pixel
-	@IN
-	D = A - D
-	@BEGIN
+(LOOP)	           //progresses the index backwards. 
+	@i
+	M=M-1
+	D=M
+	@INIT
+	D;JLT               //if index<0 - go to INDEX INITIALIZER to reset it
+	@KBD	            //loads the keyboard's address
+	D=M
+	@WHITE		        //if (Memory at keyboard address == 0) - meaning no key is pressed - go to WHITE, else go to BLACK
 	D;JEQ
-	@LOOP
+	@BLACK
+	0;JMP
+
+(BLACK)             
+	@SCREEN            //loads the screen's first address - 16384 (0x4000)
+	D=A
+	@i
+	A=D+M              //adds the current index to the screen's first address in order to color the current set of 16 pixels
+	M=-1               //sets value in current address to -1, so that the whole word 1...1 (16bits long),  meaning - all 16 pixels will be "painted" black.
+	@LOOP              //jumps back to indexer in order to progress the index backwards.
 	0;JMP
 
 (WHITE)
-	@curr
-	M = 0
-	D = M + 1
-	M = D //go to next pixel
-	@IN
-	D = A - D
-	@BEGIN
-	D;JEQ
-	@LOOP
+	@SCREEN            //loads the screen's first address - 16384 (0x4000)
+	D=A                
+	@i        
+	A=D+M              //adds the current index to the screen's first address in order to color the current set of 16 pixels
+	M=0                //sets value in current address to 0, so that the whole word will be 0....0 (16bits long), meaning - all 16 pixels will be "painted" white.
+	@LOOP           //jumps back to indexer in order to progress the index backwards.
 	0;JMP
-
-
