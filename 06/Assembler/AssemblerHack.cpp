8d477f9e84dc5f5031164a63fca8734cbc75a679
maddiehulcy <60118685+maddiehulcy@users.noreply.github.com>
2021-04-27 20:09:08 -0500

    made function to convert dec to binary to handle A instructions

--- 06/Assembler/AssemblerHack.cpp
+++ 06/Assembler/AssemblerHack.cpp
@@ -6,8 +6,11 @@
 using namespace std;
 
 char *parseLine(char *line);
+char *convertToBinary(int n);
+void reverseString(char *string, int start, int end);
 
-int main(void) {
+
+int main() {
     FILE *input_file_pointer;
     FILE *output_file_pointer;
     char *line = NULL;
@@ -20,7 +23,6 @@ int main(void) {
     output_file_pointer = fopen("C:/Users/maddi/Documents/CRCP2330/nand2tetris/projects/06/add/Add.hack", "w");
 
     getline(&line, &len, input_file_pointer);
-    cout << *line << endl;
     while (next_read != -1) {
         binary_string = parseLine(line);
         next_read = getline(&line, &len, input_file_pointer);
@@ -52,9 +54,59 @@ char *parseLine(char *line) {
 
     // if A command
     if (line[0] == '@') {
-
+        binary = convertToBinary(atoi(line + 1));
+        cout << binary << endl;
+        strcpy(res, binary);
+        free(binary);
     }
     // else C command
+    else {
+        
+    }
 
     return res;
+}
+
+
+/* Function to reverse string from start to end*/
+void reverseString(char *string, int start, int end)
+{
+    while (start < end)
+    {
+        char temp = string[start];
+        string[start] = string[end];
+        string[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+char *convertToBinary(int n)
+{
+    // array to store binary number
+    char *binaryStr;
+    binaryStr = (char*)malloc(17);
+    memset(binaryStr, '\0', 17);
+
+    // counter for binary array
+    int i = 0;
+    while (n > 0) {
+
+        // storing remainder in binary array
+        binaryStr[i] = (n % 2) + 48;
+        n = n / 2;
+        i++;
+    }
+
+    binaryStr[i] = '\0';
+
+    /* pad with zeros */
+    char *z = "0";
+    while (strlen(binaryStr) < 15) {
+        strcat(binaryStr, z);
+    }
+
+    reverseString(binaryStr, 0, 14);
+
+    return binaryStr;
 }
